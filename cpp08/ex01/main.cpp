943536f2d955b2e28cc1eb35ae1b347e8d7cbd23
rmatsuba <87314627+ryutaro3215@users.noreply.github.com>
2025-08-04 18:18:04 +0900

    commit

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -17,39 +17,41 @@
 
 
 int main(void) {
-	/* std::cout << "==== default test====" << std::endl; */
-	/* { */
-	/* 	Span sp = Span(5); */
-	/* 	sp.addNumber(6); */
-	/* 	sp.addNumber(3); */
-	/* 	sp.addNumber(17); */
-	/* 	sp.addNumber(9); */
-	/* 	sp.addNumber(11); */
-	/* 	std::cout << sp.shortestSpan() << std::endl; */
-	/* 	std::cout << sp.longestSpan() << std::endl; */
-		/* for (size_t i = 0; i < sp.getArray().size(); i++) { */
-		/* 	std::cout << sp.getArray()[i] << std::endl; */
-		/* } */
-	/* } */
-	/* { */
-	/* 	Span sp = Span(5); */
-	/* 	sp.addNumber(6); */
-	/* 	sp.addNumber(3); */
-	/* 	try { */
-	/* 		std::cout << sp.shortestSpan() << std::endl; */
-	/* 	} catch (std::exception &e) { */
-	/* 		std::cout << e.what() << std::endl; */
-	/* 	} */
-	/* 	try { */
-	/* 		std::cout << sp.longestSpan() << std::endl; */
-	/* 	} catch (std::exception &e) { */
-	/* 		std::cout << e.what() << std::endl; */
-	/* 	} */
-	/* } */
 	{
-		Span sp = Span(10);
+		std::cout << "------ First test ------" <<  std::endl;
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+		for (size_t i = 0; i < sp.getArray().size(); i++) {
+			std::cout << "Span element: " << i << " = " << sp.getArray()[i] << std::endl;
+		}
+	}
+	{
+		std::cout << "------ Second test ------" <<  std::endl;
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		try {
+			std::cout << sp.shortestSpan() << std::endl;
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+		try {
+			std::cout << sp.longestSpan() << std::endl;
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		std::cout << "------ Third test ------" <<  std::endl;
+		Span sp = Span(10000);
 		std::vector<int> vec;
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10000; i++) {
 			vec.push_back(i);
 		}
 		sp.addMultipleNumbers(vec.begin(), vec.end());
