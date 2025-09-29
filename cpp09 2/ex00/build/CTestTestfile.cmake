6b3bd7cfb4c7eeba03f0d61a58f1a488dae48338
rmatsuba <87314627+ryutaro3215@users.noreply.github.com>
2025-09-29 17:41:02 +0900

    commit

--- cpp09 2/ex00/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ryutaro320515/Documents/42Tokyo/cpp_module/cpp09/ex00
-# Build directory: /Users/ryutaro320515/Documents/42Tokyo/cpp_module/cpp09/ex00/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(bitcoin_exchange_test "/Users/ryutaro320515/Documents/42Tokyo/cpp_module/cpp09/ex00/build/bitcoin_exchange_test")
-set_tests_properties(bitcoin_exchange_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/ryutaro320515/Documents/42Tokyo/cpp_module/cpp09/ex00/CMakeLists.txt;49;add_test;/Users/ryutaro320515/Documents/42Tokyo/cpp_module/cpp09/ex00/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- cpp09 2/ex00/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ryutaro320515/Documents/42Tokyo/cpp_module/cpp09/ex00/build/_deps/googletest-src/googlemock
-# Build directory: /Users/ryutaro320515/Documents/42Tokyo/cpp_module/cpp09/ex00/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
